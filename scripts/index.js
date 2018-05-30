85e2a1a4a1b9eae3682f58bf95a4032e434348df
Lisa Dean <lisa@lisadean.net>
2018-05-30 15:02:37 -0400

    Removed map* files. Created subdirectories and added initial index.js

---
+++ scripts/index.js
@@ -0,0 +1,75 @@
+// Div where map will be created
+const MAP_CONTAINER = '[data-map]';
+// Div where a ul and li items will be created for multiple addresses returned
+// from Google
+const ADDR_SELECT_CONTAINER = '[data-address-select]';
+// Submit button
+const SUBMIT = '[data-submit]';
+// Text field where user types in zip code or address
+const ADDRESS_INPUT = '[data-address-input]';
+
+const GEO_BASE_URL = 'https://maps.googleapis.com/maps/api/geocode/json?';
+const GEO_API_KEY = 'AIzaSyBAXU-M8aiZ0Huw-2FcR0mESGdEACgBxJA';
+
+function createAddressSelectList(data) {
+  var $asc = $(ADDR_SELECT_CONTAINER);
+  var $ul = $('<ul>');
+  data.forEach(result => {
+    var formattedAddress = result.formatted_address;
+    // console.log(formattedAddress);
+    var $li = $('<li>');
+    var $a = $('<a>');
+    $a.text(formattedAddress);
+    $a.attr('href', '#');
+    $li.on('click', event => {
+      event.preventDefault();
+      $asc.text('');
+      $(ADDRESS_INPUT).attr('value', formattedAddress);
+      getCoordinates(formattedAddress)
+    });
+    $a.appendTo($li);
+    $li.appendTo($ul);
+  });
+  $ul.appendTo($asc);
+}
+
+function getCoordinates(zipCode) {
+  var url = GEO_BASE_URL + zipCode + GEO_API_KEY;
+  $.get(GEO_BASE_URL, {
+        address: zipCode,
+        key: GEO_API_KEY})
+  .then(data => {
+    if (data.results[1]) {
+      createAddressSelectList(data.results);
+    };
+    var location = data.results[0].geometry.location
+    initMap(location.lat, location.lng);
+    })
+  .catch(error => {
+    console.log(error);
+  });
+}
+
+function initMap(latValue=33.7676338,lngValue=-84.5606888) {
+  var mapDiv = document.querySelector(MAP_CONTAINER);
+  var map = new google.maps.Map(mapDiv, {
+    center: {lat: latValue, lng: lngValue},
+    zoom: 8
+  });
+  var marker = new google.maps.Marker({position: {lat: latValue, lng: lngValue},
+    map: map});
+}
+
+function submitRequest(event) {
+  event.preventDefault();
+  var zipCode = document.querySelector(ADDRESS_INPUT).value
+  if (zipCode) {
+    getCoordinates(zipCode);
+  }
+}
+
+function main() {
+  document.querySelector(SUBMIT).addEventListener('click', submitRequest);
+}
+
+main();

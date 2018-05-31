8743425d978dfe5b1e6e2a48ae39622acc0a1a6a
Dan McKinney <3dhdaniel@gmail.com>
2018-05-31 10:09:02 -0400

    js and json and csv files disregard

---
+++ oglethorpe.js
@@ -0,0 +1,81 @@
+var oglethorpe = 
+  {
+    "name": "Blazers of Lexington LLC",
+    "score": 95,
+    "address": "640 Athens Rd Lexington GA, 30648"
+  },
+  {
+    "name": "Chicken Express",
+    "score": 91,
+    "address": "643 Athens  Road Lexington GA, 30648"
+  },
+  {
+    "name": "COUNTRYSIDE CATERING",
+    "score": 99,
+    "address": "20 BUSBIN DR. WINTERVILLE GA, 30683"
+  },
+  {
+    "name": "Golden Panda",
+    "score": 94,
+    "address": "777 Athens Rd Ste. 105 Lexington GA, 30648"
+  },
+  {
+    "name": "OGLETHORPE CO. High School",
+    "score": 100,
+    "address": "749 Athens Rd LEXINGTON GA, 30648"
+  },
+  {
+    "name": "OGLETHORPE COUNTY ELEMENTARY S",
+    "score": 100,
+    "address": "LEXINGTON GA,"
+  },
+  {
+    "name": "Oglethorpe County Jail",
+    "score": 96,
+    "address": "115 Buddy Faust Road Crawford GA, 30630"
+  },
+  {
+    "name": "Oglethorpe County Middle School",
+    "score": 100,
+    "address": "270 Buddy Faust Rd Crawford GA, 30630"
+  },
+  {
+    "name": "OGLETHORPE COUNTY PRIMARY SCHOOL",
+    "score": 100,
+    "address": "300 Comer Road LEXINGTON GA, 30648"
+  },
+  {
+    "name": "OGLETHORPE COUNTY SENIOR CENT.",
+    "score": 100,
+    "address": "CRAWFORD GA, 30630"
+  },
+  {
+    "name": "PAPA'S PIZZA TO GO",
+    "score": 98,
+    "address": "1151 ATHENS RD. CRAWFORD GA, 30630"
+  },
+  {
+    "name": "QUIET OAKS HEALTH CARE",
+    "score": 94,
+    "address": "125 Quiet Oaks Dr. CRAWFORD GA, 30630"
+  },
+  {
+    "name": "Sanders Down Home BBQ",
+    "score": 99,
+    "address": "3078 Elberton Rd Carlton GA, 30627"
+  },
+  {
+    "name": "Street Stix",
+    "score": 91,
+    "address": "3334 Conger Rd Maxeys GA, 30671"
+  },
+  {
+    "name": "Subway",
+    "score": 97,
+    "address": "726 Athens Rd Lexington GA, 30648"
+  },
+  {
+    "name": "Tacos Mexican #1",
+    "score": 93,
+    "address": "777 Athens Rd Ste.  Lexington GA, 30648"
+  }
